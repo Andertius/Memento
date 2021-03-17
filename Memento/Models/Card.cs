344cfade7ccce392603e06b293722b649953bde2
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2021-03-17 20:55:41 +0200

    Card model created

--- Memento/Models/Card.cs
+++ Memento/Models/Card.cs
@@ -12,5 +12,19 @@ namespace Memento.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+
+        public ICollection<Deck> DeckID { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public long UserId { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+
+        public string Word { get; set; }
+        public string Description { get; set; }
+        public string Transcription { get; set; }
+
+        public ICollection<byte> Image { get; set; }
+        public ICollection<byte> ShirtImage { get; set; }
     }
 }
