cfa1ab1b7c7fbc23add40ba3443cb40560e6e052
psy-lock <artem.podoliak.ua@gmail.com>
2021-03-18 03:51:24 +0200

    Added few properties to Tag model

--- Memento/Models/Tag.cs
+++ Memento/Models/Tag.cs
@@ -9,6 +9,8 @@ namespace Memento.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Name { get; set; }
+        public string Description { get; set; }
+        public int TotalCards { get; set; }
 
         public ICollection<Deck> Decks { get; set; }
 
