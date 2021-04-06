7312c41ac7265b30f1d4ee571a713dd80ad2c4c1
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2021-04-06 17:12:28 +0300

    idk what ive done

--- Memento/Controllers/AccountController.cs
+++ Memento/Controllers/AccountController.cs
@@ -141,6 +141,7 @@ namespace Memento.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult ResetPassword(string token, string email)
         {
             if (token is null || email is null)
--- Memento/Controllers/HomeController.cs
+++ Memento/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace Memento.Controllers
             //you code here
             return View("Statistics");
         }
+        
     }
 }
