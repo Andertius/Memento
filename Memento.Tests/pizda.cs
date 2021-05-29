746e5199be64e00b984f6ace22f148bcd95a4a0f
Serhiy Pylypchuk <pylypchuk.serhiy@gmail.com>
2021-05-29 20:21:14 +0300

    Switch to xUnit

---
+++ Memento.Tests/pizda.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Memento.Controllers;
+using Memento.Models;
+using Memento.Models.ViewModels.BrowseDecks;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Moq;
+
+using Xunit;
+
+namespace Memento.Tests
+{
+    public class ItemsControllerTest : IClassFixture<WebTestFixture>
+    {
+        public ItemsControllerTest()
+        {
+            ContextOptions = new DbContextOptionsBuilder<MementoDbContext>()
+                   .UseInMemoryDatabase("InMemoryDbForTesting")
+                   .Options;
+        }
+
+        protected DbContextOptions<MementoDbContext> ContextOptions { get; }
+
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls) where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+            mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+            mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+
+            return mgr;
+        }
+
+        [Fact]
+        public async void Can_get_items()
+        {
+            using var context = new MementoDbContext(ContextOptions);
+            var userManager = MockUserManager(new List<User>()).Object;
+            var controller = new BrowseDecksController(userManager, context);
+
+            var user = new User { UserName = "aasda", Email = "example@gmail.com" };
+            IdentityResult result = await userManager.CreateAsync(user, "Secret123");
+
+            Assert.Equal(user.UserName, context.Users.Find(user).UserName);
+        }
+    }
+}
