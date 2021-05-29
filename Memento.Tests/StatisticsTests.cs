02838db44126d81f7c4228d68d8b94c14ffb6d73
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2021-05-29 17:45:40 +0300

    Add tests

---
+++ Memento.Tests/StatisticsTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Memento;
+
+namespace Memento.Tests
+{
+    [TestClass]
+    public class StatisticsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
