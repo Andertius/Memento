05925e565af0d307a3b02d53654549684e9e4ee2
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2021-06-04 01:07:36 +0300

    Add testing grounds

---
+++ Memento.Tests/DbFixture.cs
@@ -0,0 +1,29 @@
+﻿using Memento.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Tests
+{
+    public class DbFixture
+    {
+        public DbFixture()
+        {
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddDbContext<MementoDbContext>(options => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Memento;MultipleActiveResultSets=true"),
+                    ServiceLifetime.Transient);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ServiceProvider ServiceProvider { get; private set; }
+    }
+}
---
+++ Memento.Tests/GetStatsTest.cs
@@ -0,0 +1,47 @@
+﻿using Memento.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using Moq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Memento.Tests
+{
+    public class GetStatsTest : IClassFixture<DbFixture>
+    {
+        private UserManager<User> _userManager;
+        private ServiceProvider _serviceProvider;
+
+        public GetStatsTest(DbFixture fixture)
+        {
+            var store = new Mock<IUserStore<User>>();
+            _userManager = new UserManager<User>(store.Object, null, null, null, null, null, null, null, null);
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        [Fact]
+        public async void Get30DaysTest()
+        {
+            var context = _serviceProvider.GetService<MementoDbContext>();
+
+            var fakeUser = new User { UserName = "aasda", Email = "example@gmail.com" };
+
+            var fakePass = "checkMe123";
+
+            IdentityResult result = await _userManager.CreateAsync(fakeUser);
+
+            var check = context.Users.Where(x => x.Email == fakeUser.Email).FirstOrDefault();
+
+            Assert.NotNull(check);
+
+        }
+    }
+}
