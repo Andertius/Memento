548485f6313f33c582301e01665ea67b3255b1a6
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2021-06-04 09:37:54 +0300

    Add GetUserStats tests

--- Memento.Tests/GetStatsTest.cs
+++ Memento.Tests/GetStatsTest.cs
@@ -22,13 +22,14 @@ namespace Memento.Tests
 
         public GetStatsTest(DbFixture fixture)
         {
-            var store = new Mock<IUserStore<User>>();
+            var store = new Mock<IUserPasswordStore<User>>();
             _userManager = new UserManager<User>(store.Object, null, null, null, null, null, null, null, null);
+
             _serviceProvider = fixture.ServiceProvider;
         }
 
         [Fact]
-        public async void Get30DaysTest()
+        public void Get30DaysTest()
         {
             var context = _serviceProvider.GetService<MementoDbContext>();
 
@@ -36,11 +37,140 @@ namespace Memento.Tests
 
             var fakePass = "checkMe123";
 
-            IdentityResult result = await _userManager.CreateAsync(fakeUser);
+            context.Users.Add(fakeUser);
+            context.SaveChanges();
 
             var check = context.Users.Where(x => x.Email == fakeUser.Email).FirstOrDefault();
 
-            Assert.NotNull(check);
+            DateTime comparator = DateTime.UtcNow.AddDays(-31);
+
+            var newStats = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats);
+            context.SaveChanges();
+            //IdentityResult result = await _userManager.CreateAsync(fakeUser, fakePass);
+
+            comparator = DateTime.UtcNow.AddDays(-30);
+
+            var newStats2 = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats2);
+            context.SaveChanges();
+
+            comparator = DateTime.UtcNow.AddDays(-15);
+
+            var newStats3 = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats3);
+            context.SaveChanges();
+
+
+            var stats = new GetUserStats(context);
+            var lst = stats.GetCards(check.Id, 30);
+
+            context.Statistics.Remove(newStats);
+            context.Statistics.Remove(newStats2);
+            context.Statistics.Remove(newStats3);
+            context.SaveChanges();
+
+            context.Users.Remove(fakeUser);
+            context.SaveChanges();
+
+            Assert.Equal(3, lst.Count);
+
+        }
+
+        [Fact]
+        public void Get7DaysTest()
+        {
+            var context = _serviceProvider.GetService<MementoDbContext>();
+
+            var fakeUser = new User { UserName = "aasda", Email = "example@gmail.com" };
+
+            var fakePass = "checkMe123";
+
+            context.Users.Add(fakeUser);
+            context.SaveChanges();
+
+            var check = context.Users.Where(x => x.Email == fakeUser.Email).FirstOrDefault();
+
+            DateTime comparator = DateTime.UtcNow.AddDays(-31);
+
+            var newStats = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats);
+            context.SaveChanges();
+            //IdentityResult result = await _userManager.CreateAsync(fakeUser, fakePass);
+
+            comparator = DateTime.UtcNow.AddDays(-8);
+
+            var newStats2 = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats2);
+            context.SaveChanges();
+
+            comparator = DateTime.UtcNow.AddDays(-5);
+
+            var newStats3 = new UserStats
+            {
+                UserId = check.Id,
+                HoursPerDay = 0,
+                CardsPerDay = 0,
+                AverageHoursPerDay = 0,
+                Date = comparator,
+            };
+
+            context.Statistics.Add(newStats3);
+            context.SaveChanges();
+
+
+            var stats = new GetUserStats(context);
+            var lst = stats.GetCards(check.Id, 7);
+
+            context.Statistics.Remove(newStats);
+            context.Statistics.Remove(newStats2);
+            context.Statistics.Remove(newStats3);
+            context.SaveChanges();
+
+            context.Users.Remove(fakeUser);
+            context.SaveChanges();
+
+            Assert.Equal(2, lst.Count);
 
         }
     }
